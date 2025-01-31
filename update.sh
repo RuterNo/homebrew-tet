f71296d638556c699184f7b76f0b7f59ddd7b3c8
magnusdr-ruter <magnus.drangevag@ruter.no>
2025-01-31 12:27:58 +0100

    Fix old URL

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 cp template/tet-vehicle-display.rb Casks/tet-vehicle-display.rb
 curl --silent --remote-name https://pto-api.transhub.io/vdtools/VERSION.txt
 export VERSION=$(cat VERSION.txt)
-curl --silent --remote-name https://pto-api.stage.transhub.io/vdtools/macos/tet-vehicle-display_macos_v$VERSION.tar.gz
+curl --silent --remote-name https://pto-api.transhub.io/vdtools/macos/tet-vehicle-display_macos_v$VERSION.tar.gz
 export SHA256=$(shasum -a 256 tet-vehicle-display_macos_v$VERSION.tar.gz | awk '{print $1}')
 
 echo "Creating Cask for TET Vehicle Display v$VERSION with SHA256 $SHA256"
