1203704e11b0cdc9c5348707b223e8c69cd48f7c
magnusdr <magnusdr@users.noreply.github.com>
2025-05-05 09:36:44 +0000

    Commit from GitHub Actions (🚀 Update casks)

--- Casks/tet-vehicle-display.rb
+++ Casks/tet-vehicle-display.rb
@@ -1,7 +1,7 @@
 cask "tet-vehicle-display" do
-    version "0.22.0"
-    sha256 "03390cbe49f5008cb91b19c444a54913af386924e5d43a916a6238f0713e7f48"
-    url "https://pto-api.transhub.io/vdtools/macos/tet-vehicle-display_macos_v0.22.0.tar.gz"
+    version "0.23.0"
+    sha256 "a571dbb424774844e16ba25337cbf5cc8bba3e764e0515dc4733fd911c5e4e07"
+    url "https://pto-api.transhub.io/vdtools/macos/tet-vehicle-display_macos_v0.23.0.tar.gz"
     name "TET Vehicle Display"
     desc "Travel information on board the vehicle"
     homepage "https://vehicle-display.tet.digital"
